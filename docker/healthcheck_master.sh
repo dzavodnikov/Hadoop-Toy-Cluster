a8e5c2256b791976564f7ee5ac6485fe0769d0ec
Dmitry Zavodnikov <dzavodnikov@ripe.net>
2023-10-09 22:41:38 +0200

    Add Azkaban

--- docker/healthcheck_master.sh
+++ docker/healthcheck_master.sh
@@ -5,6 +5,7 @@ source healthcheck.sh
 check_ports 9870    # HDFS NameNode
 check_ports 8088    # YARN ResourceManager
 check_ports 19888   # Hadoop MapReduce JobHistory Server
+check_ports 8081    # Azkaban
 check_ports 16010   # HBase Web UI
 check_ports 9091    # HBase Thrift Web UI
 check_ports 8090    # HBase REST client
