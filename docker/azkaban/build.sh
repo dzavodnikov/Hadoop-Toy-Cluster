aced5f29e36bb9a2db332ce2bf819317884c4055
Dmitry Zavodnikov <dzavodnikov@ripe.net>
2025-03-21 00:54:08 +0100

    Add Azkaban distr

---
+++ docker/azkaban/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+AZKABAN_VER=4.0.0
+AZKABAN_DISTR="azkaban-${AZKABAN_VER}"
+AZKABAN_ARCH="${AZKABAN_DISTR}.tar.gz"
+
+IMAGE=azkaban-build
+CONTAINER=azkaban-build
+
+docker build --tag "${IMAGE}" .
+
+docker run --name "${CONTAINER}" --detach "${IMAGE}"
+docker cp "${CONTAINER}:${AZKABAN_ARCH}" ..
+
+docker stop "${CONTAINER}"
+docker rm   "${CONTAINER}"
