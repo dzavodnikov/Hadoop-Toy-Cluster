febce66cc38844b2d1658df86f91f1643513d06d
Dmitry Zavodnikov <dzavodnikov@ripe.net>
2025-03-21 01:00:56 +0100

    Add Spark dist (run as Standalone)

--- docker/entrypoint-worker.sh
+++ docker/entrypoint-worker.sh
@@ -30,6 +30,27 @@ hbase regionserver  start \
     &
 
 # ----------------------------------------------------------------------------------------------------------------------
+# Spark.
+# See:
+#   https://spark.apache.org/docs/3.5.5/spark-standalone.html
+# ----------------------------------------------------------------------------------------------------------------------
+# Create log dir.
+mkdir -p "${SPARK_LOG_DIR}"
+
+# Start the Worker node.
+SPARK_MASTER=spark://master1.toy:7077
+SPARK_SERVICE_PORT=7078
+SPARK_WEBUI_PORT=8081
+SPARK_WORK_DIR=/data/spark
+"${SPARK_HOME}/sbin/start-worker.sh" \
+    --host          "$(hostname -f)" \
+    --port          "${SPARK_SERVICE_PORT}" \
+    --webui-port    "${SPARK_WEBUI_PORT}" \
+    --work-dir      "${SPARK_WORK_DIR}" \
+    "${SPARK_MASTER}" \
+    > "${SPARK_LOG_DIR}/worker.log" 2>&1
+
+# ----------------------------------------------------------------------------------------------------------------------
 # Run container.
 # ----------------------------------------------------------------------------------------------------------------------
 sleep infinity
